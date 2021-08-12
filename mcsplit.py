7fed1b83ea5d97091f06df66608f3043f1d13bb0
James Trimble <james.trimble@yahoo.co.uk>
2021-08-12 15:44:12 +0100

    Add stats and fix a bug

--- mcsplit.py
+++ mcsplit.py
@@ -1,30 +1,43 @@
 import random
 import sys
 
+tmp_counter = [0]
 
 class Graph(object):
-    __slots__ = ["_node_count", "_adj_mat"]
-    def __init__(self):
-        self._node_count = 0
-        self._adj_mat = [[0 for _ in range(100)] for _ in range(100)]
+    def __init__(self, node_count = 0):
+        self._adj_mat = [[0] * node_count for _ in range(node_count)]
 
     def add_node(self):
-        self._node_count += 1
+        self._adj_mat.append([0] * len(self._adj_mat))
+        for row in self._adj_mat:
+            row.append(0)
 
     def add_edge(self, v, w):
         self._adj_mat[v][w] = True
         self._adj_mat[w][v] = True
 
     def nodes(self):
-        return list(range(self._node_count))
+        return list(range(len(self._adj_mat)))
 
     def number_of_nodes(self):
-        return self._node_count
+        return len(self._adj_mat)
 
     def adj_row(self, v):
         return self._adj_mat[v]
-#    def has_edge(self, v, w):
-#        return self._adj_mat[v][w]
+
+    def induced_subgraph(self, vv):
+        result = Graph(len(vv))
+        for i, v in enumerate(vv):
+            for j, w in enumerate(vv):
+                if self._adj_mat[v][w]:
+                    result.add_edge(i, j)
+        return result
+
+    def density(self):
+        n = self.number_of_nodes()
+        if n < 2:
+            return -1
+        return float(sum(sum(row) for row in self._adj_mat)) / (n * (n - 1))
 
 
 class LabelClass(object):
@@ -35,8 +48,6 @@ class LabelClass(object):
     a boolean which is true if and only if the nodes in the label class are
     adjacent to at least one node of the current subgraph.
     """
-
-    __slots__ = ["G_nodes", "H_nodes", "is_adjacent"]
 
     def __init__(self, is_adjacent, G_nodes, H_nodes):
         self.G_nodes = G_nodes
@@ -56,21 +67,24 @@ class PartitioningMCISFinder(object):
     def refine_label_classes(self, label_classes, v, w):
         new_label_classes = []
         for lc in label_classes:
+            G_nodes = lc.G_nodes
+            H_nodes = lc.H_nodes
             G_adj_row_v = G.adj_row(v)
             H_adj_row_w = H.adj_row(w)
-            new_lc_0 = LabelClass(
-                lc.is_adjacent,
-                [u for u in lc.G_nodes if not G_adj_row_v[u]],
-                [u for u in lc.H_nodes if not H_adj_row_w[u]]
-            )
-            new_lc_1 = LabelClass(
-                True,
-                [u for u in lc.G_nodes if G_adj_row_v[u]],
-                [u for u in lc.H_nodes if H_adj_row_w[u]]
-            )
-            for new_lc in [new_lc_0, new_lc_1]:
-                if new_lc.G_nodes and new_lc.H_nodes:
-                    new_label_classes.append(new_lc)
+            new_lc_0_G_nodes = [u for u in G_nodes if not G_adj_row_v[u]]
+            new_lc_0_H_nodes = [u for u in H_nodes if not H_adj_row_w[u]]
+            if new_lc_0_G_nodes and new_lc_0_H_nodes:
+                new_label_classes.append(LabelClass(
+                    lc.is_adjacent,
+                    new_lc_0_G_nodes,
+                    new_lc_0_H_nodes
+                ))
+            if len(new_lc_0_G_nodes) < len(G_nodes) and len(new_lc_0_H_nodes) < len(H_nodes):
+                new_label_classes.append(LabelClass(
+                    True,
+                    [u for u in G_nodes if G_adj_row_v[u]],
+                    [u for u in H_nodes if H_adj_row_w[u]]
+                ))
         return new_label_classes
 
     def select_label_class(self, label_classes, assignment_count):
@@ -86,6 +100,7 @@ class PartitioningMCISFinder(object):
         return sum(min(len(lc.G_nodes), len(lc.H_nodes)) for lc in label_classes)
 
     def search(self, label_classes, assignments, target):
+        tmp_counter[0] += 1
         if len(assignments) == target:
             self.list_of_mcs.append(dict(assignments))
             return
@@ -116,24 +131,13 @@ class PartitioningMCISFinder(object):
             return None
 
 
-def common_induced_subgraph(G, H, target, connected=False):
-    """
-    Find a common induced subgraph with at least a given number of nodes
-
-
-    This is the decision version of the :func:`max_common_induced_subgraph`
-    function.
-    """
-    return PartitioningMCISFinder(G, H, connected).find_common_subgraph(target)
-
-
 def max_common_induced_subgraph(G, H, connected=False):
     """
     Find a maximum common induced subgraph
     """
     min_n = min(G.number_of_nodes(), H.number_of_nodes())
     for target in range(min_n, -1, -1):
-        search_result = common_induced_subgraph(G, H, target, connected)
+        search_result = PartitioningMCISFinder(G, H, connected).find_common_subgraph(target)
         if search_result is not None:
             return search_result
 
@@ -144,27 +148,31 @@ if __name__ == "__main__":
         exit(1)
     max_n = int(sys.argv[1])
 
+    random.seed(1)
+
     G = Graph()
     H = Graph()
     for v in range(max_n):
         G.add_node()
         H.add_node()
-        for w in range(v - 1):
+        for w in range(v):
             if random.random() < 0.5:
                 G.add_edge(v, w)
             if random.random() < 0.5:
                 H.add_edge(v, w)
         result = max_common_induced_subgraph(G, H)
-        print("{} {} {}".format(v + 1, len(result[0]), len(result)))
-#G = Graph()
-#H = Graph()
-#G.add_node(1)
-#G.add_node(2)
-#G.add_node(3)
-#H.add_node(1)
-#H.add_node(2)
-#H.add_node(3)
-#G.add_edge(1,2)
-#G.add_edge(1,3)
-#G.add_edge(2,3)
-#print(max_common_induced_subgraph(G, H))
+        G_vtx_counts = [0] * (v + 1)
+        H_vtx_counts = [0] * (v + 1)
+        G_densities = []
+        H_densities = []
+        for m in result:
+            G_densities.append(G.induced_subgraph([t for t, u in m]).density())
+            H_densities.append(H.induced_subgraph([u for t, u in m]).density())
+            for t, u in m:
+                G_vtx_counts[t] += 1
+                H_vtx_counts[u] += 1
+        print("{:5} {:5} {:5} {} {} [{}]".format(v + 1, len(result[0]), len(result),
+                G_vtx_counts, H_vtx_counts,
+                " ".join("{:.2f}".format(d) for d in sorted(G_densities))))
+
+    print(tmp_counter)
